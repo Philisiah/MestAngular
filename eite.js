28686ca64d9332153c1f90686ecd5dd856ba4548
Philisiah <sphilisiah@gmail.com>
2018-04-07 13:26:36 +0000

    added calc date and full name functions

--- eite.js
+++ eite.js
@@ -4,10 +4,28 @@ angular.module('eite', [])
     this.lastName = "";
     this.gender = "";
     this.dob = "";
-    var currentdate = new Date();
-    var datetime = currentdate.getDate() + "-" + (currentdate.getMonth() + 1) + "-"
-      + currentdate.getFullYear() + "-" + currentdate.getHours() + "-"
-      + currentdate.getMinutes() + "-" + currentdate.getSeconds();
-    console.log(currentdate.getMilliseconds())
-
+    this.fullNames = function fullNames(){
+      return this.firstName + " " + this.lastName;
+    };
+    this.getAge = function getAge(){
+      currentYear = new Date;
+      if (this.dob) {
+        birthdate = new Date(this.dob);
+        elapsed = currentYear - birthdate;
+        elapsed = new Date(elapsed);
+        seconds = elapsed / 1000;
+        minutes = seconds / 60;
+        seconds %= 60;
+        hours = minutes/60;
+        minutes %= 60;
+        days = hours / 24;
+        hours %= 24;
+        years = days / 365;
+        days %= 365;
+        return "It has been " + years.toPrecision(3) + " years " + days.toPrecision(3) +
+         " days " + hours.toPrecision(3) + " hours " + minutes.toPrecision(3)+
+         " minutes " + seconds.toPrecision(3) + " seconds since your were born ";
+      }
+      
+    };
   });
